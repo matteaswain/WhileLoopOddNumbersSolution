6802f72ffc3f296e7e57a199846beec887ad2efa
Mattea Swain <mmswain@tql.com>
2021-05-19 14:51:50 -0400

    Add project files.

---
+++ WhileLoopOddNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhileLoopOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1;
+            while (x < 26)
+            {
+                if(x % 2 == 1)
+                {
+                    Console.WriteLine($"The cube of the {x} is {x *x*x}");
+
+                }
+
+                x++;
+            }
+        }
+    }
+}
